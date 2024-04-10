0e245b113402227f03c6e4a7e856589c90763e5f
AN-HA\AN HA <baoandlk93@gmail.com>
2024-04-10 23:46:42 +0700

    update file xml config

--- src/main/java/org/example/demospringmvc/controller/HelloController.java
+++ src/main/java/org/example/demospringmvc/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @GetMapping()
     public String index() {
         System.out.println("Hello controller");
-        return "/Home";
+        return "Home";
     }
 }
